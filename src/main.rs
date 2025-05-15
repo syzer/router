95f799a7c8e3cf616e28eb41e443f0f47ca99f6a
Lukasz Gintowt <syzer3@gmail.com>
2025-05-15 17:49:55 +0200

    WIP leds

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use esp_idf_svc::hal::prelude::*;
 use esp_idf_svc::hal::modem::Modem;
 use esp_idf_svc::wifi::*;
 use esp_idf_svc::nvs::*;
-use embedded_svc::wifi::*;
 use log::info;
-use heapless::String as HeapString;
+use esp_idf_svc::log::EspLogger;
+use esp_idf_hal::{
+    peripherals::Peripherals,
+    prelude::*,
+    rmt::{config::TransmitConfig, TxRmtDriver},
+};
+use smart_leds::{SmartLedsWrite, RGB8, colors::BLACK};     // RGB8 is here
+use ws2812_esp32_rmt_driver::LedPixelEsp32Rmt;              // new alias
+
+const LED_GPIO: i32 = 8;                 // on-board LED data pin
 
 const AP_SSID: &str = env!("AP_SSID");
 const AP_PASS: &str = env!("AP_PASS");
@@ -12,6 +19,18 @@ const AP_PASS: &str = env!("AP_PASS");
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
+
+
+    let p = Peripherals::take()?;
+
+    // Give the channel object & the GPIO pin directly:
+    let mut ws = LedPixelEsp32Rmt::new(
+        p.rmt.channel0,      // implements `Peripheral<P = CHANNEL0>`
+        p.pins.gpio8         // data pin on DevKit-C6
+    ).unwrap();              // <- Result, unwrap once
+
+    let on  = [RGB8::new(0, 0, 50)];   // dim blue
+    let off = [BLACK];                 // LED off
 
     info!("Booting up Wi-Fi AP example...");
 
@@ -45,7 +64,17 @@ fn main() -> anyhow::Result<()> {
         AP_PASS
     );
 
+    sysloop.subscribe::<WifiEvent<'static>, _>(move |ev| {
+        match ev {
+            WifiEvent::ApStaConnected { .. }   =>
+                { ws.write(on.iter().copied()).ok(); }
+            WifiEvent::ApStaDisconnected { .. }=>
+                { ws.write(off.iter().copied()).ok(); }
+            _ => {}
+        }
+    })?;;
+
     loop {
-        std::thread::sleep(std::time::Duration::from_secs(5));
+        std::thread::sleep(std::time::Duration::from_secs(60));
     }
 }
